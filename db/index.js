55d3d4f341f4036509d22aa6b181600e85f3ad06
viktorChubyrka <viktor2000897@gmail.com>
2020-07-01 15:19:56 +0300

    fix

--- db/index.js
+++ db/index.js
@@ -4,8 +4,8 @@ const config = require("../config");
 module.exports.connect = connect = () => {
   try {
     mongoose.connect(
-      "mongodb+srv://Dota2BotAdmin:dota2botadmin@cluster0-vvhtz.mongodb.net/Dota2",
-      { useNewUrlParser: true }
+      "mongodb+srv://Dota2BotAdmin:dota2botadmin@cluster0-vvhtz.mongodb.net/Dota2?retryWrites=true&w=majority",
+      { useNewUrlParser: true, useUnifiedTopology: true }
     );
   } catch (error) {
     console.log(error);
