6b4214ee77ad9480ae927c7b61aa860be989f0e8
viktorChubyrka <viktor2000897@gmail.com>
2020-07-01 14:47:43 +0300

    db-connect

--- db/index.js
+++ db/index.js
@@ -3,7 +3,10 @@ const config = require("../config");
 
 module.exports.connect = connect = () => {
   try {
-    mongoose.connect(config.MONGO_DB_URL, { useNewUrlParser: true });
+    mongoose.connect(
+      "mongodb+srv://Dota2BotAdmin:dota2botadmin@cluster0-vvhtz.mongodb.net/Dota2",
+      { useNewUrlParser: true }
+    );
   } catch (error) {
     console.log(error);
   }
