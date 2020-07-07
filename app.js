aa8fa8cc9ef5e8bc47f90e8c9e38e841a018c918
viktorChubyrka <40889994+viktorChubyrka@users.noreply.github.com>
2020-07-07 22:19:02 +0300

    pfffff.....

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const MongoStore = require("connect-mongo")(session);
 
 app.use(
   cors({
-    origin: "https://dota2gamebot.herokuapp.com",
+    origin: "http://dota2gamebot.herokuapp.com",
     credentials: true,
   })
 );
