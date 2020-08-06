e5a03a8b5fdac19e744a6e9e668258ca6546f9b5
viktorChubyrka <viktor2000897@gmail.com>
2020-08-06 17:05:10 +0300

    changet-steam-change-function

--- controller/userController/changeContactInfo.js
+++ controller/userController/changeContactInfo.js
@@ -1,8 +1,18 @@
 const User = require("../../db/models/user");
 const validation = require("../../helpers/validators");
+const Axios = require("axios");
 
 async function changeContactInfo(data) {
   let { login, loginChange, email, phone, steamID } = data;
+  let name = await Axios.get(
+    "https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=DD3E4897973E8764BB4DCE6B44F266CA&format=json&steamids=" +
+      steamID
+  );
+  let readySteamID = {
+    id: steamID,
+    name,
+  };
+  console.log(name);
   let userModel = await User.findOne({ login });
   if (userModel) {
     if (validation.isEmailValid(email)) {
@@ -12,7 +22,7 @@ async function changeContactInfo(data) {
       userModel.login = loginChange;
     }
     userModel.number = phone;
-    userModel.steamID = steamID;
+    userModel.steamID = readySteamID;
     await User.updateOne({ login }, { $set: userModel });
     return {
       data: { status: 200, message: "" },
