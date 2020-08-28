e97690417805432d995668c68a12e42775971bff
viktorChubyrka <viktor2000897@gmail.com>
2020-08-28 10:38:44 +0300

    ghjg

--- controller/userController/getUserData.js
+++ controller/userController/getUserData.js
@@ -2,6 +2,7 @@ const validation = require("../../helpers/validators");
 const User = require("../../db/models/user");
 
 async function getUserData(login) {
+  console.log(login);
   let userModel = await User.findOne({ login });
   if (!userModel)
     return {
