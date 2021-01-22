f653e6d0209d2df98b413386912cb7040c552413
viktorChubyrka <viktor2000897@gmail.com>
2021-01-22 17:59:49 +0200

    fix

--- BOT/configs/config1.js
+++ BOT/configs/config1.js
@@ -1,9 +1,9 @@
 // Configure and mv to config.js
 var config = {};
 
-config.steam_name = "SunShine";//Логин
-config.steam_user = "xcuad12345";//Логин
-config.steam_pass = "0993226219";//Пароль
+config.steam_name = "darewins";//Логин
+config.steam_user = "darewins_manager";//Логин
+config.steam_pass = "ares4696";//пароль
 config.steam_guard_code = "";
 
 
